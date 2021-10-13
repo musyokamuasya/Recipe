a164519b461a095f7a8bad6afd212c8ec708e2b0
musyokamuasya <josephmuasya8@gmail.com>
2021-10-13 16:14:06 +0300

    Chnaged library from databinding to viewbinding

---
+++ app/src/main/java/io/ciox/recipe/fragments/HomeFragment.kt
@@ -0,0 +1,25 @@
+package io.ciox.recipe.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import io.ciox.recipe.R
+
+@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/io/ciox/recipe/fragments/WebViewFragment.kt
@@ -0,0 +1,22 @@
+package io.ciox.recipe.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.ciox.recipe.R
+
+
+class WebViewFragment : Fragment(R.layout.fragment_web_view) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_web_view, container, false)
+    }
+
+
+}
