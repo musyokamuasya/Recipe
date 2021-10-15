c39b4e50319660b28584cd33dfd94f6b927890e1
musyokamuasya <josephmuasya8@gmail.com>
2021-10-15 18:36:27 +0300

    Implementaion of diffutil and asynchlist differ in recipe adapater class

--- app/src/main/java/io/ciox/recipe/adapters/RecipeAdapter.kt
+++ app/src/main/java/io/ciox/recipe/adapters/RecipeAdapter.kt
@@ -1,4 +1,48 @@
 package io.ciox.recipe.adapters
 
-class RecipeAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import io.ciox.recipe.databinding.DesignRecipeBinding
+import io.ciox.recipe.models.Result
+
+
+class RecipeAdapter:RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+
+    inner class RecipeViewHolder (val binding: DesignRecipeBinding):RecyclerView.ViewHolder(
+        binding.root
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    private val diffCallBack = object :DiffUtil.ItemCallback<Result>(){
+        override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+        override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+          return oldItem == newItem
+        }
+
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallBack)
+    var recipe: List<Result>
+        get() = differ.currentList
+        set(value){
+            differ.submitList(value)
+        }
+
 }
+
--- app/src/main/java/io/ciox/recipe/fragments/HomeFragment.kt
+++ app/src/main/java/io/ciox/recipe/fragments/HomeFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import io.ciox.recipe.R
+import io.ciox.recipe.adapters.RecipeAdapter
 import io.ciox.recipe.databinding.FragmentHomeBinding
+import io.ciox.recipe.viewmodel.RecipeViewModel
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
 private var _homeBinding:FragmentHomeBinding? = null
     private  val homeBinding get() = _homeBinding!!
+    private val recipeViewModel:RecipeViewModel by viewModels()
+    private lateinit var recipeAdapter:RecipeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
